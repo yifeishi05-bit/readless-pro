b29f078d34b2a63af8a535b63808c52848937537
yifeishi05-bit <yifeishi05@gmail.com>
2025-10-26 14:05:03 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="ReadLess Pro", page_icon="📘")
+
+st.title("📘 ReadLess Pro is Live!")
+st.subheader("Welcome to your first deployed Streamlit app 🎉")
+
+st.write("Congratulations, Alex! Your app is successfully running on Streamlit Cloud.")
+st.write("Now you can start adding features, upload PDFs, or connect AI summaries here.")
